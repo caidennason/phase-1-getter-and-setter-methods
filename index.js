b34c3fb1f0ec092c965841f61fdbd5b10f3bc956
caiden nason <caidenbnason@gmail.com>
2022-06-23 11:28:47 -0400

    commit tree 9d2b053ddc49f153bfffa55fde6d9439ca128d69

--- index.js
+++ index.js
@@ -3,8 +3,23 @@ class Circle{
     constructor(radius){
         this.radius = radius
     }
-    get diamater() { 
+    get diameter() { 
        return this.radius * 2
+    }
+    get circumference() {
+        return Math.PI * this.diameter
+    }
+    get area() {
+        return Math.PI * (this.radius * this.radius)
+    }
+    set area(newArea){
+        this.radius = Math.sqrt(newArea / Math.PI)
+    }
+    set diameter(newDiameter) {
+        this.radius = newDiameter / 2
+    }
+    set circumference(newCircumference){
+        this.radius = (newCircumference / Math.PI) / 2
     }
 }
 
